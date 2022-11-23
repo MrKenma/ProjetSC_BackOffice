d6a6da2448190e2771be8ea7ab7cb4e6bd7202e9
MrKenma <etu47233@henallux.be>
2022-11-23 22:20:05 +0100

    Création de la page "Welcome" et de la NavBar

---
+++ back_office/src/components/NavigationBar.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+
+function NavigationBar() {
+    return (
+        <div>
+            <div className="bg-info">
+                <p className="text-white text-4xl pb-1">BeSafe</p>
+            </div>
+            <div className="navbar bg-neutral">
+                <div className="navbar-start">
+                    <ul className="menu menu-horizontal p-0">
+                        <li><a>Home</a></li>
+                        <li><a>Organizations</a></li>
+                        <li><a>Events</a></li>
+                        <li><a>Users</a></li>
+                    </ul>
+                </div>
+                <div className="navbar-end">
+                    <a className="btn">Log in</a>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default NavigationBar;
---
+++ back_office/src/components/Welcome.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Welcome(props) {
+    return (
+        <div className="flex justify-center items-center h-4/5">
+            <div className="text-6xl">Welcome !</div>
+        </div>
+    )
+}
+// sans doute besoin de modifier la taille du div
+
+export default Welcome;
