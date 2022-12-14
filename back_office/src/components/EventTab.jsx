c76eeb8ac26089b3d154672f8c70a747fc35786b
WangiWeberMDS <96393683+WangiWeberMDS@users.noreply.github.com>
2022-12-14 18:21:28 +0100

    Create EventTab.jsx

---
+++ back_office/src/components/EventTab.jsx
@@ -0,0 +1,39 @@
+import React from 'react';
+import InfosButton from "./InfosButton";
+import ModifyButton from "./ModifyButton";
+
+const EventsTab = (props) => {
+    const events = props.events;
+
+    return (
+        <table className="table w-full">
+            <thead>
+            <tr>
+                <th>Name</th>
+                <th>Date and time</th>
+                <th>Adress</th>
+                <th>Organizer</th>
+                <th>Actions</th>
+            </tr>
+            </thead>
+            <tbody>
+            {events.map(event => {
+                return (
+                    <tr key={event.id}>
+                        <td>{event.name}</td>
+                        <td>{event.startdateandtime}</td>
+                        <td>{event.addresstown+", "+event.nameandnumstreet}</td>
+                        <td>{event.organizationid}</td>
+                        <td className="flex">
+                            <InfosButton id={event.id} />
+                            <ModifyButton id={event.id} />
+                        </td>
+                    </tr>
+                );
+            })}
+            </tbody>
+        </table>
+    )
+}
+
+export default EventsTab;
