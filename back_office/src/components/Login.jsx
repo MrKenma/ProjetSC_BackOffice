8b4387f8183278107b639d1254eceb85e60c9908
WangiWeberMDS <96393683+WangiWeberMDS@users.noreply.github.com>
2022-12-14 01:21:59 +0100

    Modification form de connexion

--- back_office/src/components/Login.jsx
+++ back_office/src/components/Login.jsx
@@ -1,21 +1,28 @@
 import React from 'react';
 
-function Login(){
-    return (
-        <form>
-            <div className="group">
-                <label htmlFor="email">Identifiant</label>
-                <input type="text" name="email"/>
+class Login extends React.Component{
+    render(){
+        return (
+            <div className="flex justify-center items-center">
+                <form className="bg-gray-700 w-full max-w-4xl mt-4 rounded">
+                    <div className="text-2xl mb-4 rounded-t bg-neutral pb-1">Connexion form</div>
+                    <div className="form-control max-w-sm mx-auto">
+                        <label className="label" htmlFor="name">
+                            <span className="label-text">Identifiant</span>
+                        </label>
+                        <input id="name" type="text" placeholder="ex: besafe@gmail.com" className="input" />
+                    </div>
+                    <div className="form-control max-w-sm mx-auto">
+                        <label className="label" htmlFor="name">
+                            <span className="label-text">Password</span>
+                        </label>
+                        <input id="name" type="text" placeholder="********" className="input" />
+                    </div>
+                    <button type="submit" className="btn my-4">Log In</button>
+                </form>
             </div>
-            <div className="group">
-                <label htmlFor="password">Mot de passe</label>
-                <input type="text" name="password" />
-            </div>
-            <div className="group">
-                <button>Connexion</button>
-            </div>
-        </form>
-    );
+        );
+    };
 };
 
 export default Login;
