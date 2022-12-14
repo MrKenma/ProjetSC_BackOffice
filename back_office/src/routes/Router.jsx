568d0608ccf77431fa7526a20a6a68cd8c6aea89
WangiWeberMDS <96393683+WangiWeberMDS@users.noreply.github.com>
2022-12-14 18:27:09 +0100

    modif des routes pour partier et event

--- back_office/src/routes/Router.jsx
+++ back_office/src/routes/Router.jsx
@@ -6,8 +6,8 @@ import Welcome from "../pages/Welcome";
 import Organizations from "../pages/Organizations";
 import OrganizationForm from "../pages/OrganizationForm";
 import OrganizationInfos from "../pages/OrganizationInfos";
-import Event from "../components/Event";
-import Partiers from "../components/Partiers";
+import Events from "../pages/Events";
+import Partiers from "../pages/Partiers";
 import Login from "../components/Login";
 
 export default function Router() {
@@ -17,9 +17,9 @@ export default function Router() {
             <Routes>
                 <Route path="/" element={<Welcome />} />
                 <Route path="/organizations" element={<Organizations />} />
-                <Route path="/organizationForm/:id" element={<OrganizationForm />} />
+                <Route path="/organizationForm/:id" element={<OrganizationForm />} />          
                 <Route path="/organizationInfos/:id" element={<OrganizationInfos />} />
-                <Route path="/events" element={<Event />} />
+                <Route path="/events" element={<Events />} />
                 <Route path="/partiers" element={<Partiers />} />
                 <Route path="/login" element={<Login/>}/>
             </Routes>
