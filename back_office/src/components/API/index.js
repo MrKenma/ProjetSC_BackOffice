5e394bf8233976a9d7cf08b31be8021334a6a471
WangiWeberMDS <96393683+WangiWeberMDS@users.noreply.github.com>
2022-12-14 18:20:44 +0100

    Rajout des fonction async pour events et partier

--- back_office/src/components/API/index.js
+++ back_office/src/components/API/index.js
@@ -1,5 +1,8 @@
 import axios from './http';
 
+/*-------------------------------------
+ORGANIZATIONS
+-------------------------------------*/
 export async function getOrganization(id) {
     try {
         const res = await axios.get(`organization/${id}`);
@@ -41,6 +44,105 @@ export async function updateOrganization(organization) {
 export async function deleteOrganization(id) {
     try {
         const res = await axios.delete(`organization/${id}`);
+    } catch (error) {
+        console.error(error);
+    }
+}
+
+/*-------------------------------------
+PARTIERS
+-------------------------------------*/
+export async function getPartier(id) {
+    try {
+        const res = await axios.get(`partier/${id}`);
+        return res.data;
+    } catch (error) {
+        console.log(error)
+        return [];
+    }
+}
+
+export async function getPartiers() {
+    try {
+        const res = await axios.get('partier/partiers');
+        return res.data;
+    } catch (error) {
+        console.error(error);
+        return [];
+    }
+}
+
+export async function postPartier(partier) {
+    try {
+        console.log("Post partier");
+        const res = await axios.post('partier', partier);
+    } catch (error) {
+        console.error(error);
+    }
+}
+
+export async function updatePartier(partier) {
+    try {
+        console.log("Update partier");
+        const res = await axios.patch('partier', partier);
+    } catch (error) {
+        console.error(error);
+    }
+}
+
+export async function deletePartier(id) {
+    try {
+        const res = await axios.delete(`partier/${id}`);
+    } catch (error) {
+        console.error(error);
+    }
+}
+
+/*-------------------------------------
+EVENTS
+-------------------------------------*/
+
+export async function getEvent(id) {
+    try {
+        const res = await axios.get(`event/${id}`);
+        return res.data;
+    } catch (error) {
+        console.log(error)
+        return [];
+    }
+}
+
+export async function getEvents() {
+    try {
+        const res = await axios.get('event/events');
+        return res.data;
+    } catch (error) {
+        console.error(error);
+        return [];
+    }
+}
+
+export async function postEvent(event) {
+    try {
+        console.log("Post event");
+        const res = await axios.post('event', event);
+    } catch (error) {
+        console.error(error);
+    }
+}
+
+export async function updateEvent(event) {
+    try {
+        console.log("Update event");
+        const res = await axios.patch('event', event);
+    } catch (error) {
+        console.error(error);
+    }
+}
+
+export async function deleteEvent(id) {
+    try {
+        const res = await axios.delete(`event/${id}`);
     } catch (error) {
         console.error(error);
     }
