33acde0b9918ca9c787a1aa48b33724ec43d1eff
WangiWeberMDS <96393683+WangiWeberMDS@users.noreply.github.com>
2022-12-14 18:22:57 +0100

    Create PartiersTab.jsx

---
+++ back_office/src/components/PartiersTab.jsx
@@ -0,0 +1,40 @@
+
+import React from 'react';
+import InfosButton from "./InfosButton";
+import ModifyButton from "./ModifyButton";
+
+
+const PartiersTab = (props) => {
+    const partiers = props.partiers;
+            return (
+                <table className="table w-full">
+                <thead>
+                <tr>
+                    <th>Name</th>
+                    <th>Login</th>
+                    <th>E-Mail</th>
+                    <th>Town</th>
+                    <th>Actions</th>
+                </tr>
+                </thead>
+                <tbody>
+                {partiers.map(partier => {
+                return (
+                    <tr key={partier.id}>
+                        <td>{partier.firstname+" "+partier.lastname}</td>
+                        <td>{partier.pseudo}</td>
+                        <td>{partier.emailaddress}</td>
+                        <td>{partier.addresstown+" "+partier.addresszipcode}</td>
+                        <td className="flex">
+                            <InfosButton id={partier.id} />
+                            <ModifyButton id={partier.id} />
+                        </td>
+                    </tr>
+                );
+            })}
+            </tbody>
+            </table>
+        )
+}
+
+export default PartiersTab;
