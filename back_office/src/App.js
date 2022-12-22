7a99fc68ca8389e91b181f5b17eccf9854602f72
WangiWeberMDS <96393683+WangiWeberMDS@users.noreply.github.com>
2022-12-22 01:43:06 +0100

    Update App.js

--- back_office/src/App.js
+++ back_office/src/App.js
@@ -1,11 +1,16 @@
-import React from "react";
+import React, { useState } from "react";
 import Router from "./routes/Router";
 import './App.css';
+import { hasAuthenticated } from "./services/AuthAPI";
+import Auth from "./contexts/Auth";
 
 function App() {
+  const[isAuthenticated,setIsAuthenticated]=useState(hasAuthenticated());
   return (
     <div className="App h-screen">
+      <Auth.Provider value={{isAuthenticated,setIsAuthenticated}}>
         <Router />
+      </Auth.Provider>
     </div>
   );
 }
