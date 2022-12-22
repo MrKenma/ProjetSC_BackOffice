e14c935b71115d2995732d12d8f766c90a204aaf
WangiWeberMDS <96393683+WangiWeberMDS@users.noreply.github.com>
2022-12-22 01:43:49 +0100

    Update Login.jsx

--- back_office/src/components/Login.jsx
+++ back_office/src/components/Login.jsx
@@ -1,28 +1,57 @@
-import React from 'react';
+import React,{ useEffect,useState,useContext } from 'react';
+import Auth from '../contexts/Auth';
 
-class Login extends React.Component{
-    render(){
+const Login =(history)=>{
+    const{isAuthenticated}=useContext(Auth);
+
+    const[user, setUser]=useState({
+        username:"",
+        password:""
+    })
+
+    const handleChange=({currentTarget})=>{
+        const{name,value}=currentTarget;
+
+        setUser({...user,[name]:value});
+    }
+
+    const handleSubmit=event=>{
+        event.preventDefault();
+
+        console.group();
+        console.log(user);
+        console.groupEnd()
+    }
+
+    useEffect(()=>{
+        if(isAuthenticated){
+            history.replace('/profile'); 
+        }
+
+    },[history,isAuthenticated]);
+
         return (
             <div className="flex justify-center items-center">
-                <form className="bg-gray-700 w-full max-w-4xl mt-4 rounded">
-                    <div className="text-2xl mb-4 rounded-t bg-neutral pb-1">Connexion form</div>
+                <form className="bg-gray-700 w-full max-w-4xl mt-4 rounded" onSubmit={handleSubmit}>
+                    <fieldset>
+                    <legend className="text-2xl mb-4 rounded-t bg-neutral pb-1">Connexion form</legend>
                     <div className="form-control max-w-sm mx-auto">
                         <label className="label" htmlFor="name">
                             <span className="label-text">Identifiant</span>
                         </label>
-                        <input id="name" type="email" placeholder="ex: besafe@gmail.com" className="input" />
+                        <input id="email" type="text" name="username" placeholder="ex: besafe@gmail.com" className="form-control" onChange={handleChange} />
                     </div>
                     <div className="form-control max-w-sm mx-auto">
                         <label className="label" htmlFor="name">
                             <span className="label-text">Password</span>
                         </label>
-                        <input id="name" type="password" placeholder="" className="input" />
+                        <input id="password" name="password" type="password" placeholder="" className="form-control" onChange={handleChange} />
                     </div>
                     <button type="submit" className="btn my-4">Log In</button>
+                    </fieldset>
                 </form>
             </div>
         );
-    };
 };
 
 export default Login;
